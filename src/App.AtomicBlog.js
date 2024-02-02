e1549724562f43e948338041cbd5b1115fb53f44
Lincoln Chawora <lincoln_1996@hotmail.com>
2024-02-02 19:22:00 +0100

    Refactor context system to be a lot more reusable and create custom usePosts hook to call context from anywhere

--- src/App.AtomicBlog.js
+++ src/App.AtomicBlog.js
@@ -1,6 +1,7 @@
-import {createContext, useContext, useEffect, useState} from "react";
-import { faker } from "@faker-js/faker";
+import {useEffect, useState} from "react";
 import './index.AtomicBlog.css'
+import {PostProvider, usePosts} from "./contexts/PostContext";
+import {faker} from "@faker-js/faker";
 
 function createRandomPost() {
     return {
@@ -9,32 +10,8 @@ function createRandomPost() {
     };
 }
 
-const PostContext = createContext();
-
 function AppAtomicBlog() {
-    const [posts, setPosts] = useState(() =>
-        Array.from({ length: 30 }, () => createRandomPost())
-    );
-    const [searchQuery, setSearchQuery] = useState("");
     const [isFakeDark, setIsFakeDark] = useState(false);
-
-    // Derived state. These are the posts that will actually be displayed
-    const searchedPosts =
-        searchQuery.length > 0
-            ? posts.filter((post) =>
-                `${post.title} ${post.body}`
-                    .toLowerCase()
-                    .includes(searchQuery.toLowerCase())
-            )
-            : posts;
-
-    function handleAddPost(post) {
-        setPosts((posts) => [post, ...posts]);
-    }
-
-    function handleClearPosts() {
-        setPosts([]);
-    }
 
     // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
     useEffect(
@@ -46,32 +23,26 @@ function AppAtomicBlog() {
 
     return (
         // @todo:// Create search only context.
-        <PostContext.Provider value={{
-            posts: searchedPosts,
-            onAddPost: handleAddPost,
-            onClearPosts: handleClearPosts,
-            searchQuery,
-            setSearchQuery
-        }}>
-            <section>
-                <button
-                    onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-                    className="btn-fake-dark-mode"
-                >
-                    {isFakeDark ? "☀️" : "🌙"}
-                </button>
+        <section>
+            <button
+                onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+                className="btn-fake-dark-mode"
+            >
+                {isFakeDark ? "☀️" : "🌙"}
+            </button>
 
+            <PostProvider>
                 <Header />
                 <Main />
                 <Archive />
                 <Footer />
-            </section>
-        </PostContext.Provider>
+            </PostProvider>
+        </section>
     );
 }
 
 function Header() {
-    const {onClearPosts} = useContext(PostContext);
+    const {onClearPosts} = usePosts();
     return (
         <header>
             <h1>
@@ -87,7 +58,7 @@ function Header() {
 }
 
 function SearchPosts() {
-    const {searchQuery, setSearchQuery} = useContext(PostContext);
+    const {searchQuery, setSearchQuery} = usePosts();
     return (
         <input
             value={searchQuery}
@@ -98,7 +69,7 @@ function SearchPosts() {
 }
 
 function Results() {
-    const {posts} = useContext(PostContext);
+    const {posts} = usePosts();
 
     return <p>🚀 {posts.length} atomic posts found</p>;
 }
@@ -121,7 +92,7 @@ function Posts() {
 }
 
 function FormAddPost() {
-    const {onAddPost} = useContext(PostContext);
+    const {onAddPost} = usePosts();
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
 
@@ -151,7 +122,7 @@ function FormAddPost() {
 }
 
 function List() {
-    const {posts} = useContext(PostContext);
+    const {posts} = usePosts();
 
     return (
         <ul>
@@ -166,7 +137,7 @@ function List() {
 }
 
 function Archive() {
-    const {onAddPost} = useContext(PostContext);
+    const {onAddPost} = usePosts();
     // Here we don't need the setter function. We're only using state to store these posts because the callback function
     // passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick
     // as an optimization technique, because if we just used a regular variable, these posts would be re-created on
